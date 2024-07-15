3460c10c1f4ce5c7681a4831d5a4992d88ae823a
saya4444 <smr.viola.38@gmail.com>
2024-07-16 01:16:01 +0900

    ActiveHash::Base設定

---
+++ app/models/key_id.rb
@@ -0,0 +1,20 @@
+class Key < ActiveHash::Base
+  self.data = [
+    { id:  1, name: '+6' },
+    { id:  2, name: '+5' },
+    { id:  3, name: '+4' },
+    { id:  4, name: '+3' },
+    { id:  5, name: '+2' },
+    { id:  6, name: '+1' },
+    { id:  7, name: '0'  },
+    { id:  8, name: '-1' },
+    { id:  9, name: '-2' },
+    { id: 10, name: '-3' },
+    { id: 11, name: '-4' },
+    { id: 12, name: '-5' },
+    { id: 13, name: '-6' }
+  ]
+
+  include ActiveHash::Associations
+  has_many :lists
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,57 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_07_15_134419) do
+ActiveRecord::Schema[7.0].define(version: 2024_07_15_150507) do
+  create_table "comments", charset: "utf8", force: :cascade do |t|
+    t.text "content", null: false
+    t.bigint "user_id", null: false
+    t.bigint "list_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["list_id"], name: "index_comments_on_list_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
+  end
+
+  create_table "favorites", charset: "utf8", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "list_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["list_id"], name: "index_favorites_on_list_id"
+    t.index ["user_id"], name: "index_favorites_on_user_id"
+  end
+
+  create_table "list_tags", charset: "utf8", force: :cascade do |t|
+    t.bigint "list_id", null: false
+    t.bigint "tag_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["list_id"], name: "index_list_tags_on_list_id"
+    t.index ["tag_id"], name: "index_list_tags_on_tag_id"
+  end
+
+  create_table "lists", charset: "utf8", force: :cascade do |t|
+    t.string "list_title", null: false
+    t.text "description"
+    t.string "song_title", null: false
+    t.string "reading"
+    t.integer "key_id"
+    t.string "singer"
+    t.string "link"
+    t.string "remarks"
+    t.bigint "user_id", null: false
+    t.boolean "public", default: false, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_lists_on_user_id"
+  end
+
+  create_table "tags", charset: "utf8", force: :cascade do |t|
+    t.string "tag", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", charset: "utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -28,4 +78,11 @@ ActiveRecord::Schema[7.0].define(version: 2024_07_15_134419) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "comments", "lists"
+  add_foreign_key "comments", "users"
+  add_foreign_key "favorites", "lists"
+  add_foreign_key "favorites", "users"
+  add_foreign_key "list_tags", "lists"
+  add_foreign_key "list_tags", "tags"
+  add_foreign_key "lists", "users"
 end
