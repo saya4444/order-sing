ef45c0131cc553805b22a661544659788b256be1
saya4444 <smr.viola.38@gmail.com>
2024-07-16 00:39:29 +0900

    listモデル修正

--- db/migrate/20240715150135_create_lists.rb
+++ db/migrate/20240715150135_create_lists.rb
@@ -2,16 +2,16 @@ class CreateLists < ActiveRecord::Migration[7.0]
   def change
     create_table :lists do |t|
 
-      t.string  :list_title  , null: false
-      t.text    :description
-      t.string  :song_title  , null: false
-      t.string  :reading
-      t.integer :key_id
-      t.string  :singer
-      t.string  :link
-      t.string  :remarks
-      t.integer :user_id     , null: false
-      t.boolean :public      , null: false, default: false
+      t.string     :list_title  , null: false
+      t.text       :description
+      t.string     :song_title  , null: false
+      t.string     :reading
+      t.integer    :key_id
+      t.string     :singer
+      t.string     :link
+      t.string     :remarks
+      t.references :user_id     , null: false, foreign_key: true
+      t.boolean    :public      , null: false, default: false
 
 
       t.timestamps
--- db/migrate/20240715150439_create_comments.rb
+++ db/migrate/20240715150439_create_comments.rb
@@ -2,6 +2,9 @@ class CreateComments < ActiveRecord::Migration[7.0]
   def change
     create_table :comments do |t|
 
+
+
+
       t.timestamps
     end
   end
