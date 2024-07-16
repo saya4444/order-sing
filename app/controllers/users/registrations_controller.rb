0d80cdc579c031225a7c52b2c3914d15b70e121a
saya4444 <smr.viola.38@gmail.com>
2024-07-17 00:31:28 +0900

    users controller作成

---
+++ app/controllers/users/registrations_controller.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+class Users::RegistrationsController < Devise::RegistrationsController
+
+  before_action :configure_sign_up_params, only: [:create]
+
+
+
+
+
+
+
+
+
+
+  protected
+
+  #ストロングパラメータ
+  def configure_sign_up_params
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :birthdate, :vocal_range_id, :profile, :image])
+  end
+
+
+
+
+
+
+end
