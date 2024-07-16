51e4e8eaa1d015a1bdb16590e7e9a309499409cc
saya4444 <smr.viola.38@gmail.com>
2024-07-17 00:40:53 +0900

    編集用ストロングパラメータ追加

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -3,21 +3,20 @@
 class Users::RegistrationsController < Devise::RegistrationsController
 
   before_action :configure_sign_up_params, only: [:create]
-
-
-
-
-
-
-
-
+  before_action :configure_account_update_params, only: [:update]
 
 
   protected
 
-  #ストロングパラメータ
+  #新規登録用ストロングパラメータ
   def configure_sign_up_params
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :birthdate, :vocal_range_id, :profile, :image])
   end
+
+  #編集用ストロングパラメータ
+  def configure_account_update_params
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :birthdate, :vocal_range_id, :profile, :image])
+  end
+
 
 end
