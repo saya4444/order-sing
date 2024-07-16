6c5a57535c7bf02706e16e46bd8f98c287a73162
saya4444 <smr.viola.38@gmail.com>
2024-07-17 00:06:05 +0900

    画像投稿機能追加　Active Storage

--- Gemfile
+++ Gemfile
@@ -77,3 +77,4 @@ end
 
 gem 'devise'
 gem 'active_hash'
+
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,15 +4,18 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+      # Active Storage の設定
+        has_one_attached :image
 
+      # ActiveHash の設定
         belongs_to :vocal_range, class_name: 'VocalRange', optional: true
 
-
+      # バリデーション
         validates :password        , format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i }
         validates :name            , presence: true
         validates :birthday        , presence: true
 
-
+      # アソシエーション
         has_many :lists    , dependent: :destroy
         has_many :comments , dependent: :destroy
         has_many :favorites, dependent: :destroy
