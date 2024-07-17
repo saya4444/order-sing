297582809348ebd521da0008d663d96360d7e48b
saya4444 <smr.viola.38@gmail.com>
2024-07-17 22:44:39 +0900

    userモデル　修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,7 +13,7 @@ class User < ApplicationRecord
         # バリデーション
         validates :password        , format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i }
         validates :name            , presence: true
-        validates :birthday        , presence: true
+        validates :birthdate        , presence: true
 
         # アソシエーション
         has_many :lists    , dependent: :destroy
