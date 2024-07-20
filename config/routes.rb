0462794200a1695022e614a1d4cff65b8a4415f3
saya4444 <smr.viola.38@gmail.com>
2024-07-20 22:04:33 +0900

    未ログイン時トップページ設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: 'users/registrations' }
 
+  # ログイン時 トップページを開くとログイン中のユーザーのリスト一覧ページへ
   authenticated :user do
     root 'lists#index', as: :authenticated_root
+  end
+
+  # 未ログイン時 トップページを開くとログイン画面へ
+  unauthenticated do
+    root to: redirect('/users/sign_in'), as: :unauthenticated_root
   end
 
   resources :lists
