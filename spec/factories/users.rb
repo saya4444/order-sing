f13519414558f9541ce28cbe04864e8fa7e90d92
saya4444 <smr.viola.38@gmail.com>
2024-07-21 14:13:26 +0900

    テスト　新規登録可能

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -4,10 +4,10 @@ FactoryBot.define do
     email                   {Faker::Internet.email}
     password                { Faker::Lorem.characters(number: 4) + Faker::Number.number(digits: 2).to_s }
     password_confirmation   {password}
-    birthday                { Faker::Date.birthday(min_age: 18, max_age: 65) }
+    birthdate               { Faker::Date.birthday(min_age: 18, max_age: 65) }
     vocal_range_id          { VocalRange.pluck(:id).sample }
     direct_messages_enabled { [true, false].sample }
     profile                 { Faker::Lorem.paragraph }
-    image                   { Rack::Test::UploadedFile.new(Rails.root.join('spec/fixtures/files/test_image.jpg'), 'image/jpeg') }
+    image                   { Rack::Test::UploadedFile.new(Rails.root.join('spec/fixtures/files/test_image.webp'), 'image/jpeg') }
   end
 end
