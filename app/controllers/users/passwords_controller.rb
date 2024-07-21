530ef9f1e0863f15ef83267000f263828c1460d2
saya4444 <smr.viola.38@gmail.com>
2024-07-21 12:35:58 +0900

    編集画面をdeviceのデフォルト設定に戻す
    デフォルトで編集内容の保存ができるか確認する

--- app/controllers/users/passwords_controller.rb
+++ app/controllers/users/passwords_controller.rb
@@ -1,23 +1,3 @@
 class Users::PasswordsController < Devise::PasswordsController
-  skip_before_action :authenticate_user!, only: [:edit, :update]
 
-  def edit
-  end
-
-  def update
-    @user = current_user
-
-    if @user.update_with_password(password_update_params)
-      bypass_sign_in(@user)
-      redirect_to root_path, notice: 'パスワードが更新されました。'
-    else
-      render :edit
-    end
-  end
-
-  private
-
-  def password_update_params
-    params.require(:user).permit(:current_password, :password, :password_confirmation)
-  end
 end
--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -11,22 +11,4 @@ class Users::RegistrationsController < Devise::RegistrationsController
   def configure_account_update_params
     devise_parameter_sanitizer.permit(:account_update, keys: [:name, :birthdate, :vocal_range_id, :profile, :image])
   end
-
-  def update
-    resource = resource_class.to_adapter.get!(send(:"current_#{resource_name}").to_key)
-    account_update_params = devise_parameter_sanitizer.sanitize(:account_update)
-
-    if account_update_params[:password].blank? && account_update_params[:password_confirmation].blank?
-      account_update_params.delete(:password)
-      account_update_params.delete(:password_confirmation)
-    end
-
-    if resource.update_without_password(account_update_params)
-      set_flash_message :notice, :updated
-      sign_in resource_name, resource, bypass: true
-      respond_with resource, location: after_update_path_for(resource)
-    else
-      respond_with resource
-    end
-  end
 end
