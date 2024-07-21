9e844e36048ab6afe8194975068a117cb695fc8b
saya4444 <smr.viola.38@gmail.com>
2024-07-21 22:00:43 +0900

    リスト表示設定

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,7 +1,31 @@
 class ListsController < ApplicationController
+  before_action :authenticate_user!
 
   def index
-
+    @lists = current_user.lists
+    @selected_list = List.find(params[:id]) if params[:id].present?
   end
 
+  def show
+    @list = List.find(params[:id])
+  end
+
+  def new
+    @list = List.new
+  end
+
+  def create
+    @list = current_user.lists.build(list_params)
+    if @list.save
+      redirect_to @list, notice: 'リストが作成されました。'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def list_params
+    params.require(:list).permit(:list_title, :description, :song_title, :reading, :key_id, :singer, :link, :remarks, :public)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -14,4 +14,7 @@ Rails.application.routes.draw do
   end
 
   resources :lists
+
+  resources :users, only: [:show]
+
 end
