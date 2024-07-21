daa040fcf024eef300de710a6274214a4e415e53
saya4444 <smr.viola.38@gmail.com>
2024-07-21 14:07:49 +0900

    ファクトリーボットの設定

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  before do
+    @user = FactoryBot.build(:user)
+  end
+
+  describe 'ユーザー新規登録' do
+    context '新規登録できる場合' do
+      it '必要事項を全て過不足なく入力すると登録できる' do
+        expect(@user).to be_valid
+      end
+    end
+  end
+
+
+
+
 end
